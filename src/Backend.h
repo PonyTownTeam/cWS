11a8384f6172d1aacb3e5fc3997a2ae5a38d0e70
Industrialice <iindustrialice@gmail.com>
2026-05-12 20:33:43 +0300

    Removed useless comments

--- src/Backend.h
+++ src/Backend.h
@@ -1,8 +1,6 @@
 #ifndef BACKEND_H
 #define BACKEND_H
 
-// Default to Epoll if nothing specified and on Linux
-// Default to Libuv if nothing specified and not on Linux
 #if defined(__linux__)
 	#ifndef USE_EPOLL
 		#define USE_EPOLL
