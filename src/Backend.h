8c8ef8688ae9aec9ef5b4654471108e37dd2e3b0
Industrialice <iindustrialice@gmail.com>
2026-05-12 20:32:25 +0300

    Code formatting adjustments

--- src/Backend.h
+++ src/Backend.h
@@ -4,12 +4,12 @@
 // Default to Epoll if nothing specified and on Linux
 // Default to Libuv if nothing specified and not on Linux
 #if defined(__linux__)
-#ifndef USE_EPOLL
-#define USE_EPOLL
-#endif
-#include "Epoll.h"
+	#ifndef USE_EPOLL
+		#define USE_EPOLL
+	#endif
+	#include "Epoll.h"
 #else
-#include "Libuv.h"
+	#include "Libuv.h"
 #endif
 
 #endif // BACKEND_H
